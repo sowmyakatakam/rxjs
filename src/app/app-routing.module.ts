d79cbd933e8b811b7761c7c8dc1f0a177be0d084
sowmyakatakam <sowmya.katakam111@gmail.com>
2021-02-21 23:04:29 +0530

    added promise

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './includes/header/header.component';
+import { PromiseComponent } from './promise/promise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'promise', component:PromiseComponent},
+  {path: 'header', component:HeaderComponent},
+  //{path: '**', component:PromiseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/promise/promise.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promise',
+  templateUrl: './promise.component.html',
+  styleUrls: ['./promise.component.css']
+})
+export class PromiseComponent implements OnInit {
+  count : number = 0;
+
+  constructor() {
+    console.log("in counstructor");
+   }
+ 
+
+  DellAvailable(){
+    console.log('1 entred into dellaviliable method')
+    return true;
+  }
+  HpAvailable(){
+    return false;
+  }
+  promiseVal: any;
+
+  dell = {
+    brand: 'Dell',
+    harddisk: '2 TB',
+    color: 'Black'
+  }
+
+  hp = {
+    brand: 'HP',
+    harddisk: '3 TB',
+    color: 'Sliver'
+  }
+
+  notAvil = {
+    brand: 'Not Aviliable',
+    status: 'failed'
+  }
+
+  ngOnInit(): void {
+    console.log("in ngoninit");
+    let buyLaptop = new Promise( (resolved,rejected)=>{
+      //reslove('Promise resolved')
+     // rejected('Data rejected')
+     console.log('0 before entering into if statement')
+     if(this.DellAvailable()){
+       console.log('2 below if statement condition')
+       return setTimeout(()=>{
+        //resolved('Dell is Purchased')
+        resolved(this.dell)
+       },3000)
+     }else if(this.HpAvailable()){
+        return setTimeout(()=>{
+          //resolved('HP is Purchased')
+          resolved(this.hp)
+        })
+     }else{
+       return setTimeout(()=>{
+         //rejected('No Laptop present')
+         rejected(this.notAvil)
+       })
+     }
+    });
+    
+    buyLaptop.then((res)=>{ 
+      console.log('yaa!!!  ',res)  
+      this.promiseVal = res; 
+    }).catch(res=>{
+      console.log('ayoooo!!!   ',res)
+      this.promiseVal = res;
+    })
+  }
+}
